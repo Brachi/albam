09cc4a95509e66a29448b42f797d2d7778310ce4
Sebastian Brachi <sebastianbrachi@gmail.com>
2016-03-17 14:46:03 -0300

    ALB-29: testing travis while downloading the local image

--- albam/mtframework/blender_import.py
+++ albam/mtframework/blender_import.py
@@ -49,6 +49,8 @@ def import_arc(file_path, extraction_dir=None, context_scene=None):
     # Saving arc to main object
     albam_arc = StrProp(options={'HIDDEN'}, subtype='BYTE_STRING')
     bpy.types.Object.albam_arc = albam_arc
+    import ctypes
+    print('length of arc', ctypes.sizeof(arc))
     parent.albam_arc = bytes(arc)
 
     #for i, mod_file in enumerate(mod_files):
