401e20d9be87e6ba61a3ce9231ec183e4bad1638
Sebastian Brachi <sebastianbrachi@gmail.com>
2016-03-17 14:38:41 -0300

    ALB-29: testing travis

--- albam/mtframework/blender_import.py
+++ albam/mtframework/blender_import.py
@@ -49,7 +49,7 @@ def import_arc(file_path, extraction_dir=None, context_scene=None):
     # Saving arc to main object
     albam_arc = StrProp(options={'HIDDEN'}, subtype='BYTE_STRING')
     bpy.types.Object.albam_arc = albam_arc
-    parent['albam_arc'] = bytes(arc)
+    parent.albam_arc = bytes(arc)
 
     #for i, mod_file in enumerate(mod_files):
     #    mod_dir = mod_dirs[i]
