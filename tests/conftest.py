e98e9191f2d0ba0eb4c7073cf79b54f191e24dc6
Sebastian Brachi <sebastianbrachi@gmail.com>
2016-03-20 16:13:41 -0300

    ALB-29: added arc filtering for tests, useful for test only a subset of files in a folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ SAMPLES_DIR = os.path.join(os.path.dirname(__file__), 'sample-files')
 
 
 def pytest_addoption(parser):
-    parser.addoption('--dirmod', help='Specified a custom folder for mod files (Re5)')
     parser.addoption('--dirtex', help='Specified a custom folder for tex files (Re5)')
     parser.addoption('--dirarc', help='Specified a custom folder for arc files (Re5)')
+    parser.addoption('--arcregex', help='Regex that will be applied while searching for arc files')
     parser.addoption('--blender', help='Path to Blender executable for performing functional tests')
--- tests/test_mtframework_arc.py
+++ tests/test_mtframework_arc.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import pytest
 
@@ -7,12 +8,17 @@ from tests.conftest import SAMPLES_DIR
 
 
 @pytest.fixture(scope='session')
-def arc_re5_samples(config=None):
+def arc_re5_samples():
     samples_dir = pytest.config.getoption('--dirarc') or os.path.join(SAMPLES_DIR, 're5/arc')
+    arc_regex = pytest.config.getoption('arcregex')
     CORRUPTED_ARCS = {'uOmf303.arc', 's101.arc', 'uOmS103ScrAdj.arc'}
-    return [os.path.join(root, f)
-            for root, _, files in os.walk(samples_dir)
-            for f in files if f.endswith('.arc') and f not in CORRUPTED_ARCS]
+    full_list = [os.path.join(root, f)
+                 for root, _, files in os.walk(samples_dir)
+                 for f in files if f.endswith('.arc') and f not in CORRUPTED_ARCS]
+    if arc_regex:
+        pattern = re.compile(arc_regex)
+        full_list = [f for f in full_list if pattern.search(f)]
+    return full_list
 
 
 @pytest.mark.parametrize("arc_file", arc_re5_samples())
