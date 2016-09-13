43810956b5401728107af1ac822932c432848d34
Sebastian Brachi <sebastianbrachi@gmail.com>
2016-09-12 21:34:56 -0300

    ALB-36: moved blender info to its own module. Used register_module instead of many register_class

--- albam/__init__.py
+++ albam/__init__.py
@@ -1,14 +1,9 @@
-import os
-
-from albam.mtframework.blender_import import import_arc
-from albam.mtframework.blender_export import export_arc
+from albam import blender   # noqa , used by register_module
 
 try:
     import bpy
 except ImportError:
-    from unittest.mock import Mock
-    bpy = Mock()
-
+    pass
 
 bl_info = {
     "name": "Albam",
@@ -22,89 +17,21 @@ bl_info = {
     "category": "Import-Export"}
 
 
-class AlbamImportedItemName(bpy.types.PropertyGroup):
-    '''All imported object names are saved here to then show them in the
-    export list'''
-    name = bpy.props.StringProperty(name="Imported Item", default="Unknown")
-
-
-class AlbamImportedItem(bpy.types.PropertyGroup):
-    name = bpy.props.StringProperty(options={'HIDDEN'})
-    source_path = bpy.props.StringProperty(options={'HIDDEN'})
-    folder = bpy.props.StringProperty(options={'HIDDEN'})  # Always in posix format
-    data = bpy.props.StringProperty(options={'HIDDEN'}, subtype='BYTE_STRING')
-    file_type = bpy.props.StringProperty(options={'HIDDEN'})
-
-
-class AlbamImportExportPanel(bpy.types.Panel):
-    bl_label = "Albam"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "UI"
-
-    def draw(self, context):
-        scn = context.scene
-        layout = self.layout
-        layout.operator('import.item', text='Import')
-        layout.prop_search(scn, 'albam_item_to_export', scn, 'albam_items_imported', 'select')
-        layout.operator('export.item', text='Export')
-
-
-class AlbamImportOperator(bpy.types.Operator):
-    bl_idname = "import.item"
-    bl_label = "import item"
-    directory = bpy.props.StringProperty(subtype='DIR_PATH')
-    files = bpy.props.CollectionProperty(name='adf', type=bpy.types.OperatorFileListElement)
-    filter_glob = bpy.props.StringProperty(default="*.arc", options={'HIDDEN'})
-
-    def invoke(self, context, event):
-        wm = context.window_manager
-        wm.fileselect_add(self)
-        return {'RUNNING_MODAL'}
-
-    def execute(self, context):
-        to_import = [os.path.join(self.directory, f.name) for f in self.files]
-        for item in to_import:
-            try:
-                import_arc(item, None, context.scene)
-            except Exception as err:
-                # TODO: proper logging
-                print('Error importing {}: {}'.format(item, err))
-                raise
-        return {'FINISHED'}
-
-
-class AlbamExportOperator(bpy.types.Operator):
-    bl_idname = "export.item"
-    bl_label = "export item"
-    filepath = bpy.props.StringProperty()
-
-    @classmethod
-    def poll(self, context):
-        if not bpy.context.scene.albam_item_to_export:
-            return False
-        return True
-
-    def invoke(self, context, event):
-        wm = context.window_manager
-        wm.fileselect_add(self)
-        return {'RUNNING_MODAL'}
-
-    def execute(self, context):
-        object_name = context.scene.albam_item_to_export
-        obj = bpy.data.objects[object_name]
-        arc = export_arc(obj)
-        with open(self.filepath, 'wb') as w:
-            w.write(arc)
-        return {'FINISHED'}
-
-
 def register():
-    bpy.utils.register_class(AlbamImportExportPanel)
-    bpy.utils.register_class(AlbamImportOperator)
-    bpy.utils.register_class(AlbamExportOperator)
-    bpy.utils.register_class(AlbamImportedItemName)
-    bpy.utils.register_class(AlbamImportedItem)
 
+    class AlbamImportedItemName(bpy.types.PropertyGroup):
+        '''All imported object names are saved here to then show them in the
+        export list'''
+        name = bpy.props.StringProperty(name="Imported Item", default="Unknown")
+
+    class AlbamImportedItem(bpy.types.PropertyGroup):
+        name = bpy.props.StringProperty(options={'HIDDEN'})
+        source_path = bpy.props.StringProperty(options={'HIDDEN'})
+        folder = bpy.props.StringProperty(options={'HIDDEN'})  # Always in posix format
+        data = bpy.props.StringProperty(options={'HIDDEN'}, subtype='BYTE_STRING')
+        file_type = bpy.props.StringProperty(options={'HIDDEN'})
+
+    bpy.utils.register_module(__name__)
     bpy.types.Scene.albam_item_to_export = bpy.props.StringProperty()
     bpy.types.Scene.albam_items_imported = bpy.props.CollectionProperty(type=AlbamImportedItemName)
 
@@ -121,10 +48,4 @@ def register():
 
 
 def unregister():
-    bpy.utils.unregister_class(AlbamImportExportPanel)
-    bpy.utils.unregister_class(AlbamImportOperator)
-    bpy.utils.unregister_class(AlbamExportOperator)
-
-
-if __name__ == "__main__":
-    register()
+    bpy.utils.unregister_module(__name__)
---
+++ albam/blender.py
@@ -0,0 +1,68 @@
+import os
+
+import bpy
+
+from albam.mtframework.blender_import import import_arc
+from albam.mtframework.blender_export import export_arc
+
+
+class AlbamImportExportPanel(bpy.types.Panel):
+    bl_label = "Albam"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    def draw(self, context):
+        scn = context.scene
+        layout = self.layout
+        layout.operator('import.item', text='Import')
+        layout.prop_search(scn, 'albam_item_to_export', scn, 'albam_items_imported', 'select')
+        layout.operator('export.item', text='Export')
+
+
+class AlbamImportOperator(bpy.types.Operator):
+    bl_idname = "import.item"
+    bl_label = "import item"
+    directory = bpy.props.StringProperty(subtype='DIR_PATH')
+    files = bpy.props.CollectionProperty(name='adf', type=bpy.types.OperatorFileListElement)
+    filter_glob = bpy.props.StringProperty(default="*.arc", options={'HIDDEN'})
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+    def execute(self, context):
+        to_import = [os.path.join(self.directory, f.name) for f in self.files]
+        for item in to_import:
+            try:
+                import_arc(item, None, context.scene)
+            except Exception as err:
+                # TODO: proper logging
+                print('Error importing {}: {}'.format(item, err))
+                raise
+        return {'FINISHED'}
+
+
+class AlbamExportOperator(bpy.types.Operator):
+    bl_idname = "export.item"
+    bl_label = "export item"
+    filepath = bpy.props.StringProperty()
+
+    @classmethod
+    def poll(self, context):
+        if not bpy.context.scene.albam_item_to_export:
+            return False
+        return True
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+    def execute(self, context):
+        object_name = context.scene.albam_item_to_export
+        obj = bpy.data.objects[object_name]
+        arc = export_arc(obj)
+        with open(self.filepath, 'wb') as w:
+            w.write(arc)
+        return {'FINISHED'}
