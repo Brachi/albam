93c4a22487918e5cb931b8dfbd396f4b56637be1
Sebastian Brachi <sebastianbrachi@gmail.com>
2017-05-14 14:49:21 -0300

    Add experimental concurrency while unpacking archives in generating tests for mtframework

--- tests/mtframework/conftest.py
+++ tests/mtframework/conftest.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import os
 from tempfile import mkdtemp
 import shutil
@@ -79,25 +80,45 @@ def pytest_sessionfinish(session, exitstatus):
 
 
 def _get_files_from_arcs(extension, arc_list=None, arc_path=None):
+    use_concurrency = False  # experimental
     if arc_path:
         arc_list = find_files(arc_path, '.arc')
     elif arc_list:
         arc_list = arc_list
     else:
         arc_list = ARC_FILES
+    to_export = [arc_file for arc_file in arc_list if arc_file not in CACHE_ARC]
+    if to_export and use_concurrency:
+        concurrent_unpack(arc_list, CACHE_ARC, extension)
+    elif to_export and not use_concurrency:
+        for arc_file in to_export:
+            _unpack_arc_in_temp(arc_file, CACHE_ARC)
+
+    files, ids = _get_files_and_ids(extension, arc_list)
+    return files, ids
+
+
+def _get_files_and_ids(extension, arc_list):
     files = []
     ids = []
     for arc_file in arc_list:
         files_in_arc = CACHE_ARC.get(arc_file)
         if not files_in_arc:
-            _unpack_arc_in_temp(arc_file)
-        files_in_arc = CACHE_ARC[arc_file]
+            print('arc_file missing, skipping', arc_file)
+            continue
         found_files = [f for f in files_in_arc if f.endswith(extension)]
         files.extend(found_files)
         ids_for_files = ['{}-->{}'.format(os.path.basename(arc_file), os.path.basename(f)) for f in found_files]
         ids.extend(ids_for_files)
-
     return files, ids
+
+
+def concurrent_unpack(arc_list, cache_arc, extension):
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        future_to_extracted = {executor.submit(_unpack_arc_in_temp, arc_file, CACHE_ARC) for arc_file in arc_list}
+        for i, future in enumerate(concurrent.futures.as_completed(future_to_extracted)):
+            future.result()
 
 
 def _get_array_members_from_files(files, file_ids, struct_class, array_name):
@@ -129,12 +150,14 @@ def _get_array_members_from_files(files, file_ids, struct_class, array_name):
     return array_members, ids
 
 
-def _unpack_arc_in_temp(arc_file):
+def _unpack_arc_in_temp(arc_file, arc_cache):
     tmp_dirname = os.path.basename(arc_file).replace('.arc', '-arc')
     base_temp = mkdtemp(suffix=tmp_dirname, prefix='ALBAM_')
     TEMP_DIRS_TO_DELETE.add(base_temp)
-    arc = Arc(file_path=arc_file)
+    try:
+        arc = Arc(file_path=arc_file)
+    except Exception:
+        print('error unpacking', arc_file)
+        return
     arc.unpack(base_temp)
-    CACHE_ARC[arc_file] = find_files(base_temp)
-
-    return base_temp
+    arc_cache[arc_file] = find_files(base_temp)
