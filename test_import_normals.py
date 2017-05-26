a2fd81096847507ee9d5588377e27884ba75a947
Sebastian Brachi <sebastianbrachi@gmail.com>
2017-05-26 19:36:21 -0300

    ISSUE-17: small script to reduce the problem

---
+++ test_import_normals.py
@@ -0,0 +1,49 @@
+import array
+
+import requests
+
+import bpy
+
+
+def load_mesh(vertex_locations, faces, per_vertex_normals):
+    mesh = bpy.data.meshes.new('test')
+    ob = bpy.data.objects.new('test', mesh)
+
+    mesh.from_pydata(vertex_locations, [], faces)
+
+    mesh.create_normals_split()
+
+    #for loop in mesh.loops:
+    #    loop.normal[:] = per_vertex_normals[loop.vertex_index]
+    for vert in mesh.vertices:
+        vert.normal[:] = per_vertex_normals[vert.index]
+
+    mesh.validate(clean_customdata=False)
+    mesh.update(calc_edges=True)
+
+    #clnors = array.array('f', [0.0] * (len(mesh.loops) * 3))
+    #mesh.loops.foreach_get("normal", clnors)
+
+    #mesh.normals_split_custom_set(tuple(zip(*(iter(clnors),) * 3)))
+    mesh.normals_split_custom_set_from_vertices(per_vertex_normals)
+    mesh.use_auto_smooth = True
+    mesh.show_edge_sharp = True
+
+    bpy.context.scene.objects.link(ob)
+
+    return mesh
+
+
+def main():
+    DATA_SOURCE = 'https://pastebin.com/raw/RRzmNgwd'
+    data = requests.get(DATA_SOURCE).json()
+
+    vertex_locations = data['locations']
+    faces = data['faces']
+    per_vertex_normals = data['normals']
+
+    mesh = load_mesh(vertex_locations, faces, per_vertex_normals)
+
+if __name__ == '__main__':
+    main()
+
