c0d2dbd5d198e1ef351b49d398b657c6e80da51f
HenryOfCarim <imhenryviii@gmail.com>
2023-09-11 10:25:33 +0300

    Update lmt ver51

--- albam/engines/mtfw/structs/lmt.ksy
+++ albam/engines/mtfw/structs/lmt.ksy
@@ -26,15 +26,20 @@ types:
       - {id: ofs_frame, type: u4}
       - {id: num_tracks, type: u4}
       - {id: num_frames, type: u4}
-      - {id: unk_01, type: u4, repeat: expr, repeat-expr: 25}
-      - {id: count_01, type: u4}
-      - {id: ofs_buffer, type: u4}
+      - {id: unk_01, type: f4, repeat: expr, repeat-expr: 9}
       - {id: unk_02, type: u4, repeat: expr, repeat-expr: 16}
+      - {id: count_01, type: u4}
+      - {id: ofs_buffer_01, type: u4}
+      - {id: sfx, type: u2, repeat: expr, repeat-expr: 32}
       - {id: count_02, type: u4}
-      - {id: ofs_02, type: u4}
+      - {id: ofs_buffer_02, type: u4}
     instances:
       tracks:
         {pos: ofs_frame, type: track, repeat: expr, repeat-expr: num_tracks}
+      atk_buff:
+        {pos: ofs_buffer_01, type: atk, repeat: expr, repeat-expr: count_01}
+      atk_buff2:
+        {pos: ofs_buffer_02, type: atk, repeat: expr, repeat-expr: count_02}
 
 
   track:
@@ -50,3 +55,14 @@ types:
     instances:
       data:
         {pos: ofs_data, size: len_data}
+        
+  atk:
+    seq:
+      - {id: unk_00, type: u4}
+      - {id: duration, type: u4}
+      
+  atk2:
+    seq:
+      - {id: unk_00, type: u4}
+      - {id: unk_01, type: u4}
+  
--- albam/engines/mtfw/structs/lmt.py
+++ albam/engines/mtfw/structs/lmt.py
@@ -25,28 +25,6 @@ class Lmt(KaitaiStruct):
             self.block_offsets.append(Lmt.BlockOffset(self._io, self, self._root))
 
 
-    class BlockOffset(KaitaiStruct):
-        def __init__(self, _io, _parent=None, _root=None):
-            self._io = _io
-            self._parent = _parent
-            self._root = _root if _root else self
-            self._read()
-
-        def _read(self):
-            self.offset = self._io.read_u4le()
-
-        @property
-        def block_header(self):
-            if hasattr(self, '_m_block_header'):
-                return self._m_block_header
-
-            _pos = self._io.pos()
-            self._io.seek(self.offset)
-            self._m_block_header = Lmt.BlockHeader(self._io, self, self._root)
-            self._io.seek(_pos)
-            return getattr(self, '_m_block_header', None)
-
-
     class BlockHeader(KaitaiStruct):
         def __init__(self, _io, _parent=None, _root=None):
             self._io = _io
@@ -59,17 +37,21 @@ class Lmt(KaitaiStruct):
             self.num_tracks = self._io.read_u4le()
             self.num_frames = self._io.read_u4le()
             self.unk_01 = []
-            for i in range(25):
-                self.unk_01.append(self._io.read_u4le())
+            for i in range(9):
+                self.unk_01.append(self._io.read_f4le())
 
-            self.count_01 = self._io.read_u4le()
-            self.ofs_buffer = self._io.read_u4le()
             self.unk_02 = []
             for i in range(16):
                 self.unk_02.append(self._io.read_u4le())
 
+            self.count_01 = self._io.read_u4le()
+            self.ofs_buffer_01 = self._io.read_u4le()
+            self.sfx = []
+            for i in range(32):
+                self.sfx.append(self._io.read_u2le())
+
             self.count_02 = self._io.read_u4le()
-            self.ofs_02 = self._io.read_u4le()
+            self.ofs_buffer_02 = self._io.read_u4le()
 
         @property
         def tracks(self):
@@ -84,6 +66,34 @@ class Lmt(KaitaiStruct):
 
             self._io.seek(_pos)
             return getattr(self, '_m_tracks', None)
+
+        @property
+        def atk_buff(self):
+            if hasattr(self, '_m_atk_buff'):
+                return self._m_atk_buff
+
+            _pos = self._io.pos()
+            self._io.seek(self.ofs_buffer_01)
+            self._m_atk_buff = []
+            for i in range(self.count_01):
+                self._m_atk_buff.append(Lmt.Atk(self._io, self, self._root))
+
+            self._io.seek(_pos)
+            return getattr(self, '_m_atk_buff', None)
+
+        @property
+        def atk_buff2(self):
+            if hasattr(self, '_m_atk_buff2'):
+                return self._m_atk_buff2
+
+            _pos = self._io.pos()
+            self._io.seek(self.ofs_buffer_02)
+            self._m_atk_buff2 = []
+            for i in range(self.count_02):
+                self._m_atk_buff2.append(Lmt.Atk(self._io, self, self._root))
+
+            self._io.seek(_pos)
+            return getattr(self, '_m_atk_buff2', None)
 
 
     class Track(KaitaiStruct):
@@ -116,6 +126,52 @@ class Lmt(KaitaiStruct):
             self._m_data = self._io.read_bytes(self.len_data)
             self._io.seek(_pos)
             return getattr(self, '_m_data', None)
+
+
+    class Atk(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.unk_00 = self._io.read_u4le()
+            self.duration = self._io.read_u4le()
+
+
+    class BlockOffset(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.offset = self._io.read_u4le()
+
+        @property
+        def block_header(self):
+            if hasattr(self, '_m_block_header'):
+                return self._m_block_header
+
+            _pos = self._io.pos()
+            self._io.seek(self.offset)
+            self._m_block_header = Lmt.BlockHeader(self._io, self, self._root)
+            self._io.seek(_pos)
+            return getattr(self, '_m_block_header', None)
+
+
+    class Atk2(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.unk_00 = self._io.read_u4le()
+            self.unk_01 = self._io.read_u4le()
 
 
 
