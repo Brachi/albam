9695039992dc600905878e7743529da93101512a
Sebastian A. Brachi <sebastian@brachi.org>
2024-03-18 19:48:04 -0500

    Fix outdated import per latest refactor

--- albam/engines/reng/archive.py
+++ albam/engines/reng/archive.py
@@ -2,6 +2,7 @@ import io
 import os
 import struct
 
+import bpy
 from kaitaistruct import KaitaiStream
 import pymmh3 as mmh3
 import zlib
@@ -17,27 +18,16 @@ from .structs.pak import Pak
 @blender_registry.register_archive_loader(app_id="re8", extension='pak')
 @blender_registry.register_archive_loader(app_id="re2_non_rt", extension='pak')
 @blender_registry.register_archive_loader(app_id="re3_non_rt", extension='pak')
-def pak_loader(file_item, context):
-    item_list = context.scene.albam.file_explorer.file_list
-    app_config_filepath = context.scene.albam.file_explorer.app_config_filepath
+def pak_loader(file_item):
+    app_config_filepath = bpy.context.scene.albam.file_explorer.app_config_filepath
     app_id = file_item.app_id  # blender bug, needs reference or might mutate
     if not app_config_filepath:
         # TODO: custom exception that will result in informative popup
         print("WARNING: no app_config_filepath")
         return
-    pak = PakWrapper(app_id, file_item.file_path, app_config_filepath)
-    for node in pak.tree.flatten():
-        new_item = item_list.add()
-        new_item.name = node["node_id"]
-        new_item.display_name = node["name"]
-        new_item.is_expandable = bool(node["children"])
-        new_item.app_id = app_id
-
-        new_item.tree_node.depth = node["depth"] + 1
-        new_item.tree_node.root_id = pak.tree.root_id
-        for ancestor_id in node["ancestors_ids"]:
-            ancestor_node = new_item.tree_node_ancestors.add()
-            ancestor_node.node_id = ancestor_id
+    pak = PakWrapper(app_id, file_item.absolute_path, app_config_filepath)
+    for path in pak.paths:
+        yield path.strip()
 
 
 @blender_registry.register_archive_accessor(app_id="re2", extension="pak")
@@ -72,12 +62,17 @@ class PakWrapper:
         self.file_path = file_path
         self.file_list_path = file_list_path
         self._tree = None
+        self.paths = set()
         with open(file_path, "rb") as f:
             f.seek(8)
             num_file_entries = struct.unpack("I", f.read(4))[0]
             f.seek(0)
             read_size = self.HEADER_SIZE + self.FILE_ENTRY_SIZE * num_file_entries
             self.parsed = Pak(KaitaiStream(io.BytesIO(f.read(read_size))))
+
+        with open(self.file_list_path) as f:
+            for path in f:
+                self.paths.add(path)
 
     def get_file(self, file_path):
         file_entry = None
@@ -112,6 +107,6 @@ class PakWrapper:
         tree = Tree(root_id=root_id)
         with open(self.file_list_path) as f:
             for path in f:
-                tree.add_node_from_path(path.strip(), path_separator="/")
+                tree.add_node_from_path(path.strip())
         self._tree = tree
         return self._tree
