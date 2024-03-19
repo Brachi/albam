2c1a21211728335b42e73c6e7161b8c375d601d2
Sebastian A. Brachi <sebastian@brachi.org>
2024-03-18 20:18:14 -0500

    Define order of panels, with tools at the end

--- albam/blender_ui/__init__.py
+++ albam/blender_ui/__init__.py
@@ -7,6 +7,9 @@ from .custom_properties import (
     ALBAM_PT_CustomPropertiesMesh,
 )
 from .error_handling import ALBAM_OT_ErrorHandler
+from .import_panel import ALBAM_PT_ImportSection
+from .export_panel import ALBAM_PT_ExportSection
+from .tools import ALBAM_PT_ToolsPanel, ALBAM_OT_SplitUVSeams
 
 
 __all__ = (
@@ -15,5 +18,9 @@ __all__ = (
     "ALBAM_PT_CustomPropertiesMesh",
     "ALBAM_PT_CustomPropertiesMaterial",
     "ALBAM_PT_CustomPropertiesMesh",
+    "ALBAM_PT_ImportSection",
+    "ALBAM_PT_ExportSection",
+    "ALBAM_PT_ToolsPanel",
     "ALBAM_OT_ErrorHandler",
+    "ALBAM_OT_SplitUVSeams"
 )
