ada57801744eed306a2c4521ac3b11a3f8c47292
Sebastian Aguirre Brachi <sebastian@brachi.org>
2025-06-05 17:57:22 -0500

    Initial SSG loading

--- albam/__init__.py
+++ albam/__init__.py
@@ -38,6 +38,7 @@ def register():
     importlib.import_module(".engines.mtfw.archive", __package__)
     importlib.import_module(".engines.mtfw.mesh", __package__)
     importlib.import_module(".engines.mtfw.navmesh", __package__)
+    importlib.import_module(".engines.hexn.archive")
     if os.getenv("ALBAM_ENABLE_REEN"):
         importlib.import_module(".engines.reng.archive", __package__)
         importlib.import_module(".engines.reng.mesh", __package__)
